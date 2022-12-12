5ba6627db4116ebaf98262bd679b9e3a3876f04e
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-12 15:12:32 +0100

    add img from vue

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 createApp({
   data(){
     return{
-      message : "Hello vue!"
+      message : "Hello vue!",
+      img : "./img/vue.jpg",
+      alt : "vue",
+      dimension : "w-50"
     }
   }
 }).mount("#app")
